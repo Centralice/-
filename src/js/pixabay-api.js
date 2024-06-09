7c7cccc5ae51ddde4f62c1997b8216f521ea28c4
Centralice <108938330+Centralice@users.noreply.github.com>
2024-06-09 18:24:30 +0200

    init

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -10,49 +10,7 @@ import { createMarkup } from '/js/render-functions';
 
 const loader = document.querySelector('.loader');
 
-// export function getImages(query) {
-//   const BASE_URL = 'https://pixabay.com/api/';
-//   const params = new URLSearchParams({
-//     key: '44273329-392765d5a069e216bf7d20a4c',
-//     q: query,
-//     image_type: 'photo',
-//     orientation: 'horizontal',
-//     safesearch: true,
-//   });
-//   const url = `${BASE_URL}?${params}`;
 
-//   loader.classList.add('is-on');
-
-//   return fetch(url)
-//     .then(res => res.json())
-//     .then(data => {
-//       if (!data.hits.length) {
-//         iziToast.error({
-//           message: 'Sorry, there are no images matching your search query',
-//           messageColor: '#ffffff',
-//           backgroundColor: '#0077b6',
-//           position: 'topRight',
-//           timeout: 5000,
-//         });
-//       } else {
-//         const container = document.querySelector('.container');
-//         createMarkup(data.hits);
-//         container.insertAdjacentHTML('beforeend', createMarkup(data.hits));
-
-//         const lightbox = new SimpleLightbox('.container a', {
-//           captionsData: 'alt',
-//           captionDelay: 250,
-//         });
-//         lightbox.refresh();
-//       }
-//     })
-//     .catch(error => {
-//       console.log(error);
-//     })
-//     .finally(() => {
-//       loader.classList.remove('is-on');
-//     });
-// }
 
 export async function getImages(query) {
   const params = {
--- src/main.js
+++ src/main.js
@@ -8,24 +8,6 @@ const container = document.querySelector('.container');
 
 formEl.addEventListener('submit', handleSearch);
 
-// function handleSearch(event) {
-//   event.preventDefault();
-
-//   container.innerHTML = '';
-//   const query = event.target.elements.search.value.trim();
-//   if (!query) {
-//     iziToast.error({
-//       message: 'Ay caramba! No empty fields, please',
-//       messageColor: '#ffffff',
-//       backgroundColor: '#d00000',
-//       position: 'topRight',
-//       timeout: 5000,
-//     });
-//   } else {
-//     getImages(query);
-//     formEl.reset();
-//   }
-// }
 
 async function handleSearch(event) {
   event.preventDefault();
